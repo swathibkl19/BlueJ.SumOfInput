79f5e417e66ca660e3bd6b92d305248ffffd551e
Swathi Bikumalia <swathibikumalia@zipcoders-MacBook-Pro.local>
2019-02-06 17:32:51 -0500

    Implemented Sumofinput and tested

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
-/**
+import java.util.*;/**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        int number,sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+         number=sc.nextInt();
+        
+        for(int i=0;i<=number;i++)
+        {
+        sum=i+sum;
+    }
+    System.out.println("The sum of numbers"+ sum);
+        
 
     }
 }
